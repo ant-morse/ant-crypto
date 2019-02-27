f2147124cfd7fbb50d5f50be4751906492a2ffa2
wenzhao.fw <wenzhao.fw@alibaba-inc.com>
2019-02-27 10:34:01 +0800

    remove surplus comments

--- examples/mini-program/pages/aes/aes.js
+++ examples/mini-program/pages/aes/aes.js
@@ -2,8 +2,6 @@ import onTap from '/utils/aes';
 
 Page({
   onLoad() {
-    // my.alert({ title: `typeof crypto`, content: typeof crypto })
-    // my.alert({ title: `typeof getRandomValues`, content: typeof getRandomValues })
   },
   data: {
     plainText: 'This is a plain text',
